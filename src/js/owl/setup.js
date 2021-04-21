51151071bbb409dabe193baf08a2c39467c08cf7
leo-allves <leonardoalves1992@outlook.com>
2021-04-21 11:43:55 -0300

    Initial commit

---
+++ src/js/owl/setup.js
@@ -0,0 +1,18 @@
+//código do carrosel simples usando OWL CAROUSEL2
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        } 
+    }
+})
